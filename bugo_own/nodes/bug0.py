73c9782cfc634ae97381197500402307d7c85435
prabakaranxyz <93246374+prabakaranxyz@users.noreply.github.com>
2021-12-20 11:12:56 +0530

    update

--- bugo_own/nodes/bug0.py
+++ bugo_own/nodes/bug0.py
@@ -7,6 +7,7 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from tf import transformations
 from std_srvs.srv import *
+import roscpp
 
 # Global variables
 robot_pos = Point()
